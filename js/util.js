855c3b6240aa6aa967d7cfd2cdcfd72bdeea2c28
owbfk <artem.volchihin@mail.ru>
2023-10-17 15:34:16 +0500

    модуляция 1

---
+++ js/util.js
@@ -0,0 +1,29 @@
+import {messages} from '/js/data.js';
+import {names} from '/js/data.js';
+import {descriptions} from '/js/data.js';
+
+const createComment = function(id){
+  return {
+    id: id,
+    avatar: `img/avatar--${Math.floor(Math.random() * 5)+1}.svg`,
+    message: messages[Math.floor(Math.random() * messages.length)],
+    name: names[Math.floor(Math.random() * names.length)]
+  };
+};
+
+const createPhoto = function(id){
+  let comments = [];
+  for (let i = 0; i < Math.floor(Math.random() * 30); i++){
+    comments[i] = createComment(toString(i) + toString(id));
+  }
+  return {
+    id: id,
+    url: `photos/${toString(id)}.jpg`,
+    description: descriptions[Math.floor(Math.random() * descriptions.length)],
+    likes: Math.floor(Math.random() * 176) + 15,
+    comments: comments
+  };
+};
+
+export {createPhoto};
+export{createComment};
