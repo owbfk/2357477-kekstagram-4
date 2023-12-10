e769b6ca60cf485b8ef2f0b1f59416f14dd107ea
owbfk <artem.volchihin@mail.ru>
2023-12-10 18:00:49 +0500

    /

--- js/bigPicture.js
+++ js/bigPicture.js
@@ -1,6 +1,7 @@
+/* eslint-disable*/
+
 function bigPicture(picture) {
   let big = document.querySelector('.big-picture');
-  console.log(big);
   picture.addEventListener('click', function(){
     big.hidden = false;
 
